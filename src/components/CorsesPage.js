72352695c96e7372b9a183dd627e1a496ee272dd
SuHamza <suzan.hamza@gmail.com>
2020-04-11 01:19:38 +0200

    Controller Views & PropTypes

--- src/components/CorsesPage.js
+++ src/components/CorsesPage.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
+
 // Convert Class component into a function
 function CoursesPage() {
     // Declare state using useState Hook
@@ -19,30 +21,7 @@ function CoursesPage() {
     return (
         <>
             <h2>Courses</h2>
-            <table className='table'>
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Author ID</th>
-                        <th>Category</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        // Iterate over the courses array and
-                        // return each course's info
-                        courses.map(course => {
-                            return (
-                                <tr key={course.id}>
-                                    <td>{course.title}</td>
-                                    <td>{course.authorId}</td>
-                                    <td>{course.category}</td>
-                                </tr>
-                            );
-                        })
-                    }
-                </tbody>
-            </table>
+            <CourseList courses={courses} />
         </>
     );
 }
