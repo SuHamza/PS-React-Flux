2f9536882bc107dc6cfe93d4f0e38bfe6a53d8d9
SuHamza <suzan.hamza@gmail.com>
2020-04-13 19:07:27 +0200

    Saving Data

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import CourseForm from './CourseForm';
+import * as courseApi from '../api/courseApi';
 
 const ManageCoursePage = props => {
     // Declare an empty course state
@@ -18,10 +19,17 @@ const ManageCoursePage = props => {
         // according to the event name
         setCourse({ ...course, [target.name]: target.value });
     }
+
+    // Save Courses
+    function handleSubmit(event) {
+        // Prevent the page from posting back to the server
+        event.preventDefault();
+        courseApi.saveCourse(course);
+    }
     return (
         <>
             <h2>Manage Course</h2>
-            <CourseForm course={course} onChange={handleChange} />
+            <CourseForm course={course} onChange={handleChange} onSubmit={handleSubmit} />
         </>
     );
 }
